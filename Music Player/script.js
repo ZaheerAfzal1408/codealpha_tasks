c05c4e308ffb7a25e8076fb351e555606219443d
Muhammad Zaheer Afzal <129364686+ZaheerAfzal1408@users.noreply.github.com>
2024-07-15 16:02:43 +0500

    Update script.js

--- Music Player/script.js
+++ Music Player/script.js
@@ -4,6 +4,7 @@ const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
+const songList = document.getElementById('song-list');
 const albumImg = document.getElementById('album-img');
 
 let isPlaying = false;
@@ -11,16 +12,35 @@ let isPlaying = false;
 const songs = [
     {
         title: 'Gore Gore Mukhde Pe Ishq',
-        artist: 'Vishk Rebound',
+        artist: 'Vishk',
         src: '/songs/Gore Gore Mukhde Pe Ishq Vishk Rebound 128 Kbps.mp3',
         img: '/images/Gore Gore Mukhde Pe.png'
     },
     {
         title: 'Ishq Vishk Pyaar Vyaar',
-        artist: 'Vishk Rebound',
+        artist: 'Vishk',
         src: '/songs/Ishq Vishk Pyaar Vyaar Ishq Vishk Rebound 128 Kbps.mp3',
         img: '/images/Ishq Vishk Pyaar Vyaar.png'
     },
+    {
+        title: 'Husan Tera Toba Toba',
+        artist: 'Karan Aujla',
+        src: '/songs/Husan-Tera-Toba-Toba(DJSathi.Me).mp3',
+        img: '/images/Husn Tera Toba Toba.png'
+    },
+    {
+        title: 'Amplifier',
+        artist: 'Imran Khan',
+        src: '/songs/Amplifier - Imran Khan 128 Kbps.mp3',
+        img: '/images/Amplifier.png'
+    },
+    {
+        title: 'Aja We Mahiya',
+        artist: 'Imran Khan',
+        src: '/songs/Aaja We Mahiya - Unforgettable 128 Kbps.mp3',
+        img: '/images/Aja We Mahiya.png'
+    }
+    
     // Add more songs here
 ];
 
@@ -44,7 +64,6 @@ function playPauseMusic() {
     isPlaying = !isPlaying;
 }
 
-
 function prevSong() {
     currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
     loadSong(songs[currentSongIndex]);
@@ -61,8 +80,25 @@ function nextSong() {
     playPauseButton.innerHTML = '<i class="fas fa-pause"></i> Pause';
 }
 
+function populateSongList() {
+    songs.forEach((song, index) => {
+        const songItem = document.createElement('div');
+        songItem.classList.add('songlist-item');
+        songItem.innerHTML = `<span>${song.title}</span><span>${song.artist}</span>`;
+        songItem.addEventListener('click', () => {
+            currentSongIndex = index;
+            loadSong(song);
+            audio.play();
+            isPlaying = true;
+            playPauseButton.innerHTML = '<i class="fas fa-pause"></i> Pause';
+        });
+        songList.appendChild(songItem);
+    });
+}
+
 playPauseButton.addEventListener('click', playPauseMusic);
 prevButton.addEventListener('click', prevSong);
 nextButton.addEventListener('click', nextSong);
 
 loadSong(songs[currentSongIndex]);
+populateSongList();
